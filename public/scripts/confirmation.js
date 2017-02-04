c876d039543129e6061753a27aaea739ab9a5006
David Kim <davidjinuk90@gmail.com>
2017-02-04 20:57:19 +0000

    Created confirmation page to pull orders from in memory database

---
+++ public/scripts/confirmation.js
@@ -0,0 +1,41 @@
+$(() => {
+
+  function loadData() {
+      $.ajax ({
+        //change to get after database is set
+        method: "POST",
+        url: "/api/cart",
+        success: function (cartData) {
+          for(let orderID in cartData){
+            console.log(cartData[orderID]);
+            renderData(cartData[orderID]);
+          }
+        }
+      });
+    }
+
+  function renderData(arrOfObjects) {
+    arrOfObjects.forEach(function(object){
+      $(".all-items").append(createCartItem(object))
+    })
+
+  }
+
+  function createCartItem (object){
+    let $item = `
+      <tr>
+        <td data-th="Product">
+          <div class="row">
+            <div class="col-sm-10">
+              <h4 class="nomargin">${object.name}</h4>
+                <p>${object.description}</p>
+            </div>
+            <td data-th="Quantity" class="text-center">${object.item_quantity}</td>
+      </tr>
+    `;
+    return $item;
+  }
+
+  loadData();
+
+});
